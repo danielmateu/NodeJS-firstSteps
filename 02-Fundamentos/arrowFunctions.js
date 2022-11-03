7adf8e54daf41f34f2885c0ce43b6d060943e9f8
Daniel Mateu <danielmateu86@gmail.com>
2022-11-03 15:04:31 +0100

    funciones flecha

---
+++ 02-Fundamentos/arrowFunctions.js
@@ -0,0 +1,19 @@
+
+
+// const suma = (num1 = 0, num2 = 0) => {
+    // return num1 + num2;
+// }
+
+/**
+ * If num1 and num2 are not provided, they default to 0. Then, return the sum of num1 and num2.
+ * @param [num1=0] - 0
+ * @param [num2=0] - 0
+ */
+const suma = (num1 = 0, num2 = 0) => num1 + num2;
+/**
+ * It returns the string 'Hola mundo'
+ */
+const saludar = () => 'Hola mundo';
+
+console.log(suma(5,10))    
+console.log(saludar())    
