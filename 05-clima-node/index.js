9d4c9fa4fa95387886940e6ce10b5267c1989713
Daniel Mateu <danielmateu86@gmail.com>
2022-11-06 20:06:36 +0100

    desinstalamos color para instalar colors y la version correcta de inquirer - 7.3.3

--- 05-clima-node/index.js
+++ 05-clima-node/index.js
@@ -1,4 +1,15 @@
+const { leerInput } = require("./helpers/inquirer")
 
 
 
-console.log('Hola Mundo!');
+
+
+const main = async() => {
+
+    const texto = await leerInput('Hola: ');
+
+    console.log(texto);
+
+}
+
+main();
